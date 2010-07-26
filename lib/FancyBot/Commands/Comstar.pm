cc0696cc4d7670b4ef1790b824070c3611740c4b
Pausenclown <holli.holzer@googlemail.com>
2010-07-26 07:06:21 +0200

    bugfix

--- lib/FancyBot/Commands/Comstar.pm
+++ lib/FancyBot/Commands/Comstar.pm
@@ -8,45 +8,55 @@ sub execute
 	my $self    = shift;
 	my $bot     = shift;
 	my $user    = shift;
-	my $topic   = quotemeta( shift );
+	my $topic   = quotemeta( shift ); 
 	
-	my @topics = ( @{ $bot->config->{ComStar}->{Gun} }, @{ $bot->config->{ComStar}->{Mech} } );
+	my @topics = ( @{ $bot->config->{Comstar}->{Gun} }, @{ $bot->config->{Comstar}->{Mech} } );
 	
-	my @foundx = grep { $_->{Name} =~ /^${topic}$/i } @topics;
-	my @found  = grep { $_->{Name} =~ /$topic/i } @topics;
-	
-	if ( !@foundx && @found > 1 ) 
+	for my $key ( qw( SubType Type Name ) )
 	{
-		$bot->send_chatter( "Multiple items found: ". join( ', ', map { $_->{Name} } @found ) );
-		return;
-	}
 	
-	if ( !@foundx && !@found  ) 
-	{
-		$bot->send_chatter( "No items found." );
-		return;
-	}
-	
-	@found = @foundx if @foundx;
-	
-	if ( ref $found[0]->{Slots} )
-	{
-		$found[0]->{Slots} = join ', ', 
-			map { $_." (". $found[0]->{Slots}->{$_}. ")" } 
-			grep { !ref $found[0]->{Slots}->{$_} } 
-			keys %{ $found[0]->{Slots} };
-	}
-	
-	if ( ref $found[0]->{Electronics} )
-	{
-		$found[0]->{Electronics} = join ', ', 
-			grep { $found[0]->{Electronics}->{$_}  } 
-			keys %{ $found[0]->{Electronics} };
-	}
-	
-	$bot->send_chatter( join( '; ', map { "$_: ". $found[0]->{$_} } grep { $_ ne 'Name' } keys %{$found[0]} ) );
+		my @foundx = grep { $_->{$key} ? $_->{$key} =~ /^${topic}$/i : 0 } @topics;
+		my @found  = grep { $_->{$key} ? $_->{$key} =~ /$topic/i : 0  } @topics; 
+		
+		if ( @foundx == 0 && @found > 1 ) 
+		{
+			$bot->send_chatter( "Multiple items found: ". join( ', ', map { $_->{Name} } @found ) );
+			return;
+		}
 
+		next
+			if !@foundx && !@found; 
+			
+		my $found = @foundx ? $foundx[0] : $found[0];
+		
+		if ( ref $found->{Slots} )
+		{
+			$found->{Slots} = join ', ', 
+				map { $_." (". $found->{Slots}->{$_}. ")" } 
+				grep { !ref $found->{Slots}->{$_} } 
+				keys %{ $found->{Slots} };
+		}
+		
+		if ( ref $found->{Electronics} )
+		{
+			$found->{Electronics} = join ', ', 
+				grep { $found->{Electronics}->{$_}  } 
+				keys %{ $found->{Electronics} };
+		}
+		
+		$bot->send_chatter( join( '; ', map { "$_: ". $found->{$_} } grep { $_ ne 'Name' } keys %{$found} ) );
+		
+		my @other = grep { $_ !~ /^${topic}$/i } map { $_->{Name} } @found;
+		
+		$bot->send_chatter( "Other items found: ". join( ', ',  @other ) )
+			if @other;
+		
+		return 1;
+	}
+	
+	$bot->send_chatter( "No items found." );
 	return 1;
+	
 }
 
 1;
